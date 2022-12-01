4ed3a51eac48e8e0fcbb1b666ab963769862311a
Stuchuck <malinka27032004@gmail.com>
2022-12-01 12:41:28 +0200

    PR 6_6

--- main.js
+++ main.js
@@ -1,78 +1,64 @@
 import Pokemon from "./pokemon.js";
-import { $getElById, random } from "./utils.js";
+import { getElById, random, countClicks, changeButtonName } from './utils.js';
+import { pokemons } from "./pokemons.js";
+
+const pokemonsVariants = [...pokemons];
+
+function getRandomPlayer() {
+    return pokemonsVariants.splice(random(pokemonsVariants.length) - 1, 1)[0];
+}
 
 const player1 = new Pokemon({
-    name: 'Pikachu',
-    type: 'electric',
-    hp: 500,
+    ...getRandomPlayer(),
     selectors: 'character',
 });
 
 const player2 = new Pokemon({
-    name: 'Charmander',
-    type: 'fire',
-    hp: 450,
+    ...getRandomPlayer(),
     selectors: 'enemy',
 });
 
+const $control = document.querySelector('.control');
+
+player1.attacks.forEach(item => {
+    console.log(item);
+    const $btn = document.createElement('button');
+    $btn.classList.add('button');
+
+    $btn.textContent = `${item.name} (${item.maxCount})`;
+    const attack = kick(item, $btn);
+    $btn.addEventListener('click', () => {
+        console.log('Click button', $btn.innerText);
+        attack();
+    });
+
+    $control.appendChild($btn);
+});
+
+function kick({ minDamage: player1_MinDamage, maxDamage: player1_MaxDamage, maxCount }, btn) {
+    const remainClicks = countClicks(maxCount);
+
+    return () => {
+        const { minDamage: player2_MinDamage, maxDamage: player2_MaxDamage } = player2.attacks[random(player2.attacks.length) - 1];
+
+        generateLog(player2, player1, player2.changeHP(random(player1_MinDamage, player1_MaxDamage)));
+        generateLog(player1, player2, player1.changeHP(random(player2_MinDamage, player2_MaxDamage)));
+
+        const clicksNumber = remainClicks();
+
+        btn.textContent = changeButtonName(btn, clicksNumber);
+
+        if (!clicksNumber) btn.disabled = true;
+
+        if (!player1.hp.current || !player2.hp.current) {
+            document.querySelectorAll('.button').forEach(button => button.disabled = true);
+        }
+    }
+}
+
 const $logs = document.querySelector('#logs');
 
-const $btn = $getElById('btn-thunder-jolt');
-const $btn2 = $getElById('btn-electro-ball');
-
-const btnCountJolt = countBtn(6, $btn);
-$btn.addEventListener('click', () => {
-    btnCountJolt();
-    player1.changeHP(random(60, 20), function(count) {
-        console.log('Some change after change HP', count);
-        console.log(generateLog(player1, player2, count)); 
-    });
-    player2.changeHP(random(60, 20), function(count) {
-        console.log('Some change after change HP', count);
-    });
-});
-
-const btnElectroBall = countBtn(10, $btn2);
-$btn2.addEventListener('click', () => {
-    btnElectroBall();
-    player1.changeHP(random(20), function(count) {
-        console.log('Some change after change HP', count); 
-    });
-    player2.changeHP(random(20), function(count) {
-        console.log(generateLog(player2, player1, count)); 
-        console.log('Some change after change HP', count);
-    });
-});
-
-function countBtn(count = 6, el) {
-    const innerText = el.innerText;
-    el.innerText = `${innerText} (${count})`;
-    return () => {
-        count--;
-        console.log(count);
-        if(count === 0) {
-            el.disabled = true;
-        }
-        el.innerText = `${innerText} (${count})`;
-        return count;
-    }
-}
-
-function changeHP(count) {
-    this.hp.current -= count;
-
-    const log = this === enemy ? generateLog(this, character, count) : generateLog(this, enemy, count);
-    //console.log(log);
-    if(this.hp.current <= 0) {
-        this.hp.current = 0;
-        alert('Бедный '+ this.name +' проиграл бой!');
-        $btn.disabled = true;
-    }
-
-    this.renderHP();
-}
-
-/*function generateLog(character, enemy, count) {
+function generateLog(character, enemy, count) {
     const logs = [
         `${character.name} вспомнил что-то важное, но неожиданно ${enemy.name}, не помня себя от испуга, ударил в предплечье врага.`,
         `${character.name} поперхнулся, и за это ${enemy.name} с испугу приложил прямой удар коленом в лоб врага.`,
@@ -92,24 +78,15 @@ function changeHP(count) {
     $p.innerText = `${log} -${count} [${character.hp.current}/${character.hp.total}]`;
     $logs.insertBefore($p, $logs.children[0]);
     $logs.scrollTop = 0;
-}*/
-
-function generateLog(player1, player2, count) {
-    const { name, hp: { current, total } } = player1;
-    const { name: enemyName } = player2;
-
-    const logs = [
-        `${name} вспомнил что-то важное, но неожиданно ${enemyName}, не помня себя от испуга, ударил в предплечье врага. -${count}, [${current}/${total}]`,
-        `${name} поперхнулся, и за это ${enemyName} с испугу приложил прямой удар коленом в лоб врага. -${count}, [${current}/${total}]`,
-        `${name} забылся, но в это время наглый ${enemyName}, приняв волевое решение, неслышно подойдя сзади, ударил. -${count}, [${current}/${total}]`,
-        `${name} пришел в себя, но неожиданно ${enemyName} случайно нанес мощнейший удар. -${count}, [${current}/${total}]`,
-        `${name} поперхнулся, но в это время ${enemyName} нехотя раздробил кулаком \<вырезанно цензурой\> противника. -${count}, [${current}/${total}]`,
-        `${name} удивился, а ${enemyName} пошатнувшись влепил подлый удар. -${count}, [${current}/${total}]`,
-        `${name} высморкался, но неожиданно ${enemyName} провел дробящий удар. -${count}, [${current}/${total}]`,
-        `${name} пошатнулся, и внезапно наглый ${enemyName} беспричинно ударил в ногу противника. -${count}, [${current}/${total}]`,
-        `${name} расстроился, как вдруг, неожиданно ${enemyName} случайно влепил стопой в живот соперника. -${count}, [${current}/${total}]`,
-        `${name} пытался что-то сказать, но вдруг, неожиданно ${enemyName} со скуки, разбил бровь сопернику. -${count}, [${current}/${total}]`
-    ];
-
-    return logs[random(logs.length)-1];
 }
+
+function init() {
+    console.log('Start Game!');
+
+    document.querySelector('.character img').src = player1.img;
+    document.querySelector('.enemy img').src = player2.img;
+    getElById('name-character').textContent = player1.name;
+    getElById('name-enemy').textContent = player2.name;
+}
+
+init();
--- pokemon.js
+++ pokemon.js
@@ -6,45 +6,54 @@ class Selectors {
 }
 
 class Pokemon extends Selectors {
-    constructor({ name, hp, type, selectors }) {
+    constructor({ name, hp, type, selectors, img, attacks }) {
         super(selectors);
 
+        this.img = img;
         this.name = name;
         this.hp = {
             current: hp,
             total: hp,
         };
         this.type = type;
+        this.attacks = attacks;
 
         this.renderHP();
     }
 
-    changeHP = (count, cb) => {
+    changeHP = (count) => {
         this.hp.current -= count;
-    
-        if(this.hp.current <= 0) {
+
+        if (this.hp.current <= 0) {
             this.hp.current = 0;
+            alert(this.name + ' проиграл!');
         }
-    
+
         this.renderHP();
-        cb && cb(count);
+        return count;
     }
 
     renderHP = () => {
         this.renderHPLife();
         this.renderProgressbarHP();
     }
-    
+
     renderHPLife = () => {
         const { elHP, hp: { current, total } } = this;
-    
-        elHP.innerText = current + ' / '+ total;
+        elHP.textContent = current + ' / ' + total;
     }
-    
+
     renderProgressbarHP = () => {
         const { hp: { current, total }, elProgressbar } = this;
-        const procent = current / (total / 100);
+        const procent = (current / total) * 100;
         elProgressbar.style.width = procent + '%';
+
+        if (procent < 60 && procent > 20) {
+            elProgressbar.classList.add('low');
+        }
+        else if (procent < 20) {
+            elProgressbar.classList.add('critical');
+        }
     }
 }
 
